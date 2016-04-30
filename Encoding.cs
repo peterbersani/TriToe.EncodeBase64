415ce8778f28d37dd1a6d5a14dbe55a136117fd4
Peter Bersani <peter.bersani@gmail.com>
2016-04-30 16:40:52 -0400

    Initial Code Commit

---
+++ Encoding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace TriToe.EncodeBase64
+{
+    public class Encoder
+    {
+        public string ConvertToBase64(string filePath)
+        {
+            using (Image image = Image.FromFile(filePath))
+            {
+                var format = image.RawFormat;
+                return ConvertToBase64(image, format);
+            }
+        }
+
+        public string ConvertToBase64(Image image, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //First Convert Image to byte[]
+                image.Save(ms, format);
+                // Check that image is less than 75k. It's a bad idea to base64 encode big images.
+                if (ms.Length <= 76800)
+                {
+                    byte[] imageBytes = ms.ToArray();
+
+                    //Get Image MimeType
+                    var codec = ImageCodecInfo.GetImageDecoders().First(c => c.FormatID == format.Guid);
+                    var mimeType = codec.MimeType;
+
+                    //Then Convert byte[] to Base64 String
+                    string base64String = string.Format("data:{0};base64,{1}", mimeType, Convert.ToBase64String(imageBytes));
+                    return base64String;
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ EventHandlers.cs
@@ -0,0 +1,61 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace TriToe.EncodeBase64
+{
+    public class RegisterEvents : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            MediaService.Saving += MediaService_Saving;
+        }
+
+        private void MediaService_Saving(IMediaService sender, SaveEventArgs<IMedia> e)
+        {
+            foreach (var mediaItem in e.SavedEntities)
+            {
+                if (mediaItem.ContentType.Alias == "triToeBase64Image" && mediaItem.HasProperty("triToeBase64EncodedImage") && mediaItem.HasProperty("umbracoFile"))
+                {
+                    // Check that image is less than 75k. It's a bad idea to base64 encode big images.
+                    var umbracoBytes = mediaItem.GetValue<int>("umbracoBytes");
+                    if (umbracoBytes <= 76800)
+                    {
+                        var umbracoFile = mediaItem.GetValue("umbracoFile");
+                        var base64 = string.Empty;
+                        if (umbracoFile != null)
+                        {
+                            var uFile = umbracoFile.ToString().Trim();
+
+                            // Checking for presence of json. This will be true if this is a crop rather than an upload file.
+                            if (uFile.StartsWith("{") && uFile.EndsWith("}"))
+                            {
+                                dynamic d = JsonConvert.DeserializeObject(uFile);
+                                uFile = d.src;
+                            }
+
+                            if (!string.IsNullOrEmpty(uFile))
+                            {
+                                var filePath = HttpContext.Current.Server.MapPath(uFile);
+                                var encoder = new Encoder();
+                                base64 = encoder.ConvertToBase64(filePath);
+                            }
+                        }
+
+                        if (string.IsNullOrEmpty(base64) && mediaItem.GetValue("triToeBase64EncodedImage") != null)
+                        {
+                            mediaItem.SetValue("triToeBase64EncodedImage", null);
+                        }
+                        else
+                        {
+                            mediaItem.SetValue("triToeBase64EncodedImage", base64);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
